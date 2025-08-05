14158588e8820028632a76519efc3fc3f3e55803
go1106 <xinyingjia@Xinyings-MacBook-Pro.local>
2025-08-05 15:00:01 -0500

    add average rating

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,9 +2,16 @@ from rest_framework import serializers
 from .models import Movie, Rating   
 
 class MovieSerializer(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
     class Meta:
         model = Movie
-        fields = ['movieId', 'title', 'genres']
+        fields = ['movieId', 'title', 'genres', 'average_rating']
+    def get_average_rating(self, obj):
+        ratings = Rating.objects.filter(movie=obj)
+
+        if ratings.exists():
+            return round(sum(rating.rating for rating in ratings) / len(ratings),2)
+        return None
 
 class RatingSerializer(serializers.ModelSerializer):
     movie = MovieSerializer(read_only=True)
