7c50cdd46e798093cc88356d66e25b0ab6335557
go1106 <xinyingjia@Xinyings-MacBook-Pro.local>
2025-08-07 13:53:18 -0500

    filter/search/order function3, move from client side to server side

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render
-from django.db.models import Avg
+from django.db.models import Avg, Value
 
 from rest_framework import viewsets, filters
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+from django_filters import rest_framework as djfilters
+from django.db.models.functions import Coalesce
+
+class MoviFilter(djfilters.FilterSet):
+    genres = djfilters.CharFilter(field_name='genres', lookup_expr='icontains')
+    average_rating = djfilters.NumberFilter(field_name='average_rating', lookup_expr='gte')
+
+    class Meta:
+        model = Movie
+        fields = ['genres', 'average_rating']   
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.annotate(
-        average_rating=Avg('rating__rating'))
-        
-    serializer_class = MovieSerializer 
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+        average_rating=Coalesce(Avg('rating__rating'),Value(0.0)))
+    serializer_class = MovieSerializer
+
+    # Enable filtering, searching, and ordering
+
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = MoviFilter
+    
+    #filterset_fields = ['genres','average_rating']  # Allow filtering by year and genres
     search_fields = ['title']  # Assuming Movie model has 'title' and 'description' fields
+    
     ordering_fields = ['title', 'average_rating', 'year']  # Allow ordering by these fields
     ordering = ['-average_rating']  # Default ordering by title   
     #pagination_class = None  # Disable pagination for simplicity, can be customized as needed
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
